9f519a9b0f140baafa077665876705a036173a38
ChenPi11 <102936596+ChenPi11@users.noreply.github.com>
2023-02-01 20:01:59 +0800

    Add files via upload

---
+++ elang.h
@@ -0,0 +1,74 @@
+#ifndef e
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif
+#define e auto
+#define ee void
+#define eee char
+#define eeee unsigned
+#define eeeee wchar_t
+#define eeeeee short
+#define eeeeeee int
+#define eeeeeeee long
+#define eeeeeeeee float
+#define eeeeeeeeee double
+#define eeeeeeeeeee signed
+#define eeeeeeeeeeee struct
+#ifdef __cplusplus
+#define eeeeeeeeeeeee class
+#else
+#define eeeeeeeeeeeee struct
+#endif
+#define eeeeeeeeeeeeee public
+#define eeeeeeeeeeeeeee private
+#define eeeeeeeeeeeeeeee protected
+#define eeeeeeeeeeeeeeeee const
+#define eeeeeeeeeeeeeeeeeeee mutable
+#define eeeeeeeeeeeeeeeeeeeee register
+#define eeeeeeeeeeeeeeeeeeeeee volatile
+#define eeeeeeeeeeeeeeeeeeeeeee static
+#define eeeeeeeeeeeeeeeeeeeeeeee sizeof
+#define eeeeeeeeeeeeeeeeeeeeeeeee namespace
+#define eeeeeeeeeeeeeeeeeeeeeeeeee true
+#define eeeeeeeeeeeeeeeeeeeeeeeeeee false
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeee bool
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeee "3.1415926589793238461145141919810249534957342857239875329087509834738902783059750984273t980r23f892y305t9ygf2398yf20894235try904723fu098tyffyr98uf089fy"
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeee this
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee typedef
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee using
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee virtual
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee union
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee enum
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee extern
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee friend
+#define eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee operator
+//do nothing
+#define $ 
+#define $$ break
+#define $$$ case
+#define $$$$ catch
+#define $$$$$ continue
+#define $$$$$$ delete
+#define $$$$$$$ delete[]
+#define $$$$$$$$($e) delete[$e]
+#define $$$$$$$$$ do
+#define $$$$$$$$$$ for
+#define $$$$$$$$$$$ if
+#define $$$$$$$$$$$$ new
+#define $$$$$$$$$$$$$ return
+#define $$$$$$$$$$$$$$ switch
+#define $$$$$$$$$$$$$$$ throw
+#define $$$$$$$$$$$$$$$$ try
+#define $$$$$$$$$$$$$$$$$ while
+#define $$$$$$$$$$$$$$$$$$ default
+
+#define fk main
+#define you ()
+#define nvi__a int
+
+//version
+#define __e 114514L
+
+#include <stdio.h>
+#define hello_world() printf("hello word!\n")
+#endif
